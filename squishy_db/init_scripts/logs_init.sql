46c7e35e83cd9450671333d0de6ee22c62c1b7fd
joshdlowell <joshua.d.lowell@gmail.com>
2025-06-27 21:15:57 -0400

    Aligned log levels with the rest of squishy service

--- squishy_db/init_scripts/logs_init.sql
+++ squishy_db/init_scripts/logs_init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS logs (
     log_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     site_id VARCHAR(5) DEFAULT ('local'), -- Not case sensitive
-    log_level ENUM('ERROR', 'STATUS', 'WARNING', 'INFO') DEFAULT ('INFO'), -- Not case sensitive
+    log_level ENUM('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL') DEFAULT ('INFO'), -- Not case sensitive
     timestamp INT UNSIGNED DEFAULT (UNIX_TIMESTAMP()),
     summary_message TEXT NOT NULL,
     detailed_message TEXT

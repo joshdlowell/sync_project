dfad978a27ff8f53514ffd9f78e720f3728503c2
joshdlowell <joshua.d.lowell@gmail.com>
2025-06-27 21:11:33 -0400

    cleaned up imports

--- squishy_REST_API/configuration/__init__.py
+++ squishy_REST_API/configuration/__init__.py
@@ -1,6 +1,3 @@
 from .config import config, logger
-# from .logging_config import logger
-# from .config import RESTConfig
-# from .logging_config import RESTLogger
 
 __all__ = ['config', 'logger']

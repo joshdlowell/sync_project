f1936447f6c312df30c853bb82210e8603c353ab
jlowell <joshua.d.lowell@gmail.com>
2025-07-02 21:16:43 -0400

    Added support for Gui routes and coreDB interface (if core)

--- squishy_REST_API/app_factory/app_factory.py
+++ squishy_REST_API/app_factory/app_factory.py
@@ -7,7 +7,7 @@ with proper configuration and dependency injection.
 from flask import Flask
 
 from squishy_REST_API import config, logger
-from squishy_REST_API.database_client import DBClient
+from squishy_REST_API.database_client import DBClient, CoreDBClient
 from .api_routes import register_api_routes
 from .gui_routes import register_gui_routes
 
@@ -27,10 +27,13 @@ class RESTAPIFactory:
         Returns:
             Configured Flask application
         """
-        # Get db instance
+        # Get local db instance
         db_instance = (DBClient()).database_client
+        # Get cored db instance
+        core_db_instance = (CoreDBClient()).database_client if config.is_core else None
         # Create Flask app (with location of web-gui templates)
-        app = Flask(__name__, template_folder='../web/templates', static_folder='../web/static')
+        app = Flask(__name__, template_folder='../web/templates', static_folder='../web/static') \
+            if config.is_core else Flask(__name__)
 
         # Load configuration
         if test_config:
@@ -48,7 +51,8 @@ class RESTAPIFactory:
 
         # Register routes
         register_api_routes(app, db_instance)
-        register_gui_routes(app, db_instance)
+        if config.is_core:
+            register_gui_routes(app, core_db_instance)
 
         # Log application startup
         logger.info(f"Application created with DEBUG={app.config['DEBUG']}")
